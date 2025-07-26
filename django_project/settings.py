8ccbbcaffcc5cfbea41a2abe27d78b7d49ba495e
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2025-07-26 18:06:18 +0500

    registration from updated, forms.py file made

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- django_project/urls.py
+++ django_project/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
-
 ]
